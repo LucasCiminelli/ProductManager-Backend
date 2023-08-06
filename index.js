c4e450e00ce59eac7c4e7f18fab06be772acfb9a
Lucas Ciminelli <lucasaciminelli@gmail.com>
2023-08-06 10:53:25 -0300

    Product Manager

--- index.js
+++ index.js
@@ -1,80 +1,93 @@
 class ProductManager {
-    products;
-    static id = 0;
+  products;
+  static id = 0;
 
-    constructor(){
-        this.products = []
-    }
-    
-    getProducts(){
-        return this.products;
-    }
+  constructor() {
+    this.products = [];
+  }
 
-    addProduct(title, description, price, thumbnail, code, stock){
+  getProducts() {
+    return this.products;
+  }
 
-        const product = {
-            id:ProductManager.id,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-            
-        };
+  addProduct(title, description, price, thumbnail, code, stock) {
+    const product = {
+      id: ProductManager.id,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
 
-        if (product.title === undefined || product.id === undefined || product.description === undefined || product.price === undefined || product.thumbnail === undefined || product.code === undefined || product.stock === undefined){
-            console.error ("Faltan completar campos");
-            return;
-        }
-
-        this.products.push(product);
-        ProductManager.id++;
-        return product;
-
-    }
-
-    getProducts(){
-        return this.products;
-    }
-    
-    getProductsById(id){
-        const foundProduct = this.products.find((prod) => prod.id === id);
-        if (!foundProduct){
-            console.error("Error, el ID ingresado no coincide con ningun producto. Ingrese un ID válido")
-            return;
-        }
-        return console.log("El id ingresado corresponde al siguiente producto: ", foundProduct);
+    if (
+      product.title === undefined ||
+      product.id === undefined ||
+      product.description === undefined ||
+      product.price === undefined ||
+      product.thumbnail === undefined ||
+      product.code === undefined ||
+      product.stock === undefined
+    ) {
+      console.error("Faltan completar campos");
+      return;
     }
 
-    getProductRepetido(code){
-        const repetido = this.products.find((prod)=> prod.code === code)
-        if(repetido){
-            console.log("Producto repetido", repetido);
-            return repetido;
-        }
+    this.products.push(product);
+    ProductManager.id++;
+    return product;
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductsById(id) {
+    const foundProduct = this.products.find((prod) => prod.id === id);
+    if (!foundProduct) {
+      console.error(
+        "Error, el ID ingresado no coincide con ningun producto. Ingrese un ID válido"
+      );
+      return;
     }
+    return console.log(
+      "El id ingresado corresponde al siguiente producto: ",
+      foundProduct
+    );
+  }
 
-
-} 
+  getProductRepetido(code) {
+    const repetido = this.products.find((prod) => prod.code === code);
+    if (repetido) {
+      console.log("Producto repetido", repetido);
+      return repetido;
+    }
+  }
+}
 
 const manager = new ProductManager();
 
-manager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
-manager.addProduct("producto prueba2", "Este es un producto prueba2", 200, "Sin imagen2", "abc123", 10);
+manager.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "Sin imagen",
+  "abc123",
+  25
+);
+manager.addProduct(
+  "producto prueba2",
+  "Este es un producto prueba2",
+  200,
+  "Sin imagen2",
+  "abc123",
+  10
+);
 
-
-
-
-const verProducts = manager.getProducts()
-console.log("Products:",verProducts);
+const verProducts = manager.getProducts();
+console.log("Products:", verProducts);
 
 const productoBuscado = manager.getProductsById(4);
 
 const productoRepetido = manager.getProductRepetido("abc123");
-
-
-
-
-
-
